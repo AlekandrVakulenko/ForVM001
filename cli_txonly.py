e37f984a5b162ab9334bd4d0885414c6cc252fc8
AlexandrVakulenko <sasha705@mail.ru>
2021-03-09 16:12:14 +0300

    client side added

---
+++ cli_txonly.py
@@ -0,0 +1,16 @@
+from socket import *
+# HOST = '127.0.0.1'
+HOST = '62.109.23.127'
+PORT = 3000
+BUFSIZE = 1024
+SOCKADDR = (HOST, PORT)
+uCliSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    data = input('>')
+    if not data:
+        break
+    uCliSock.sendto(data.encode(), SOCKADDR)
+    if data == 'q':
+        break
+
+uCliSock.close()
